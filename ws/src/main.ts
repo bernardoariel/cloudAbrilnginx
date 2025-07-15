900ea350fd8244b169607a063363e0dc0b88625e
Ariel Bernardo <arielbernardo@hotmail.com>
2025-07-15 16:07:04 -0300

    Agregando cambios

--- ws/src/main.ts
+++ ws/src/main.ts
@@ -6,8 +6,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  
+  // Habilitar CORS
+  app.enableCors({
+    origin: true, // Permite todas las origenes en desarrollo
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3010);
 }
 bootstrap();
   
